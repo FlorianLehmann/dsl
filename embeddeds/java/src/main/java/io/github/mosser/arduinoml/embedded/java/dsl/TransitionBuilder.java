6b4984a13073d8ed0dbc78b3ff32c5d6f49e6259
FlorianLehmann <florian_lehmann@yandex.com>
2019-01-25 10:12:44 +0100

    add basic scenario

--- embeddeds/java/src/main/java/io/github/mosser/arduinoml/embedded/java/dsl/TransitionBuilder.java
+++ embeddeds/java/src/main/java/io/github/mosser/arduinoml/embedded/java/dsl/TransitionBuilder.java
@@ -1,42 +1,46 @@
 package io.github.mosser.arduinoml.embedded.java.dsl;
 
 
+import io.github.mosser.arduinoml.kernel.behavioral.SignalTransition;
+import io.github.mosser.arduinoml.kernel.behavioral.State;
+import io.github.mosser.arduinoml.kernel.behavioral.TimeTransition;
 import io.github.mosser.arduinoml.kernel.behavioral.Transition;
 import io.github.mosser.arduinoml.kernel.structural.SIGNAL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransitionBuilder {
 
 
     private TransitionTableBuilder parent;
-
     private Transition local;
+    private String source;
 
     TransitionBuilder(TransitionTableBuilder parent, String source) {
         this.parent = parent;
-        this.local = new Transition();
-        parent.findState(source).setTransition(local);
+        this.source = source;
     }
 
-
-    public TransitionBuilder when(String sensor) {
-        local.setSensor(parent.findSensor(sensor));
-        return this;
+    public TransitionSignalBuilder when(String sensor) {
+        SignalTransition signalTransition = new SignalTransition();
+        this.local = signalTransition;
+        parent.findState(source).addTransition(local);
+        signalTransition.setSensor(parent.findSensor(sensor));
+        return new TransitionSignalBuilder(this, signalTransition);
     }
 
-    public TransitionBuilder isHigh() {
-        local.setValue(SIGNAL.HIGH);
-        return this;
-    }
-
-    public TransitionBuilder isLow() {
-        local.setValue(SIGNAL.LOW);
+    public TransitionBuilder after(long time) {
+        TimeTransition timeTransition = new TimeTransition();
+        this.local = timeTransition;
+        parent.findState(source).addTransition(local);
+        timeTransition.setTime(time);
         return this;
     }
 
     public TransitionTableBuilder goTo(String state) {
-        local.setNext(parent.findState(state));
+        this.local.setNext(parent.findState(state));
         return parent;
     }
-
 
 }
