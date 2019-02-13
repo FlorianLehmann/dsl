c7942101f43443381fddaf229c6dad8470dbbfbe
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2019-02-13 10:39:57 +0100

    Added connection to serial port

--- embeddeds/python/pyArduinoML/visualization/main.py
+++ embeddeds/python/pyArduinoML/visualization/main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+
+import serial
+
+
+def initArduino(serialPort):
+    try:
+        print("Trying to connect on serial port " + serialPort)
+        ser = serial.Serial(serialPort)
+        print("Connection successful")
+        return ser
+    except:
+        newSerialPort = input("Connection failed. Input the name of your serial port and press enter :")
+        return initArduino(newSerialPort)
+
+
+if __name__ == "__main__":
+    serial = initArduino("dev/ttyUSB0")
